a9541c1a2f37d3ff7b641c4d276fc6f3be3fff1a
Matheus Leal <matheusleal1301@gmail.com>
2024-11-30 10:09:33 -0300

    Adiciona Recuperação de dados

--- Secao_21/_02_jdbc2/src/db/DB.java
+++ Secao_21/_02_jdbc2/src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -39,15 +41,29 @@ public class DB {
 		}
 	}
 
-	private static Properties loadProperties() {
-		// db.properties funciona porque arquivo está na pasta raiz do projeto
+	public static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
-			// Carregar propriedades que estão no db.properties
-			props.load(fs); // load faz a leitura do arquivo apontado pela stream fs e armazena os dados no obj props
+			props.load(fs);
 			return props;
 		}
 		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		try {
+			st.close();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		try {
+			rs.close();
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
